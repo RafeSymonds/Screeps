cfe390f20f0751a416238cf303193811b381fe9a
Rafe <rsymonds@umich.edu>
2025-12-27 15:21:49 -0500

    fixed issues with creep assignment

--- src/tasks/TaskAssignment.ts
+++ src/tasks/TaskAssignment.ts
@@ -2,27 +2,25 @@ import { World } from "world/World";
 import { AnyTask, Task } from "./Task";
 import { TaskManager } from "./TaskManager";
 
-function isCreepFree(creep: Creep, taskManager: TaskManager): boolean {
-    return creep.memory.taskId === undefined || !taskManager.tasks.has(creep.memory.taskId);
+function isCreepFree(creepMemory: CreepMemory, taskManager: TaskManager): boolean {
+    return creepMemory.taskId === undefined || !taskManager.tasks.has(creepMemory.taskId);
 }
 
 export function assignCreeps(world: World) {
     for (const [, room] of world.rooms) {
         for (const creep of room.myCreeps) {
-            if (!isCreepFree(creep.creep, world.taskManager)) {
+            if (!isCreepFree(creep.memory, world.taskManager)) {
                 continue;
             }
-
-            console.log(room.tasks, room.tasks.size);
 
             for (const taskId of room.tasks) {
                 const task = world.taskManager.get(taskId);
 
-                console.log("lk;jasdfj;klsaf", task, taskId);
-
                 if (task) {
                     creep.memory.taskId = task.id();
                     task.assignCreep(creep.creep);
+
+                    break;
                 }
             }
         }
