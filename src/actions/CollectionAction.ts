1c0f2fcab286711d938122c14cf14c773683708c
Rafe <rsymonds@umich.edu>
2025-12-27 14:35:20 -0500

    collect target

--- src/actions/CollectionAction.ts
+++ src/actions/CollectionAction.ts
@@ -1,5 +1,22 @@
 import { Action } from "./Action";
 
 export class CollectAction extends Action {
-    public override perform(creep: Creep): void {}
+    target: Resource | Structure | Tombstone | Ruin;
+
+    constructor(target: Resource) {
+        super();
+        this.target = target;
+    }
+
+    public override perform(creep: Creep): void {
+        if (this.target instanceof Resource) {
+            if (creep.pickup(this.target) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(this.target);
+            }
+        } else {
+            if (creep.withdraw(this.target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(this.target);
+            }
+        }
+    }
 }
