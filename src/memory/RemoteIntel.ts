787d53d3ea340f318a0057b686f52acc14c841f3
Rafe <rsymonds@umich.edu>
2025-12-28 16:38:24 -0500

    remote intel start

---
+++ src/memory/RemoteIntel.ts
@@ -0,0 +1,19 @@
+type RemoteRoomIntel = {
+    roomName: string;
+
+    // Static
+    sources: Id<Source>[];
+    mineral?: Id<Mineral>;
+    exits: string[];
+
+    // Semi-static
+    controller?: {
+        id: Id<StructureController>;
+        owner?: string;
+        reservedBy?: string;
+    };
+
+    // Safety
+    hostileOwner: boolean;
+    lastSeen: number;
+};
